1859b65098c0b18a95dc3ce4a2cde10170a44a09
oleg-rabaev <oleg@pubsubsql.com>
2014-02-12 11:43:12 -0500

    QuickStart add sql rules comments

--- QuickStart/QuickStart.go
+++ QuickStart/QuickStart.go
@@ -10,7 +10,7 @@ import (
 func checkError(client *pubsubsql.Client, str string) {
 	if client.Failed() {
 		fmt.Println("Error:", client.Error(), str)
-	}	
+	}
 }
 
 func main() {
@@ -18,31 +18,48 @@ func main() {
 	subscriber := new(pubsubsql.Client)
 
 	//----------------------------------------------------------------------------------------------------
-	// CONNECT 
+	// CONNECT
 	//----------------------------------------------------------------------------------------------------
 
-	address := "localhost:7777"	
+	address := "localhost:7777"
 	client.Connect(address)
 	checkError(client, "client connect failed")
-	subscriber.Connect(address)	
+	subscriber.Connect(address)
 	checkError(client, "subscriber connect failed")
 
 	//----------------------------------------------------------------------------------------------------
-	// INDEX 
-	// only optional ddl commands available are 
-	// key (unique index) and/or tag (non unique index)
-	// table and columns are auto created
+	// SQL MUST-KNOW RULES
+	//
+	// All commands must be in lower case.
+	//
+	// Identifiers can only begin with alphabetic characters and may contain any alphanumeric characters.
+	//
+	// The only available (but optional) data definition commands are
+	//    key (unique index)      - key table_name column_name
+	//    tag (non-unique index)  - tag table_name column_name
+	//
+	// Tables and columns are auto created when accessed.
+	//
+	// The underlying data type for all columns is string.
+	// The strings do not have to be enclosed in single quotes as long as they have no special characters.
+	// The special characters are
+	//    , - comma
+	//      - white space characters (space, tab, new line)
+	//    ) - right parenthesis
+	//    ' - single quote
 	//----------------------------------------------------------------------------------------------------
-	
-	// Creates key (unique index) for table Stocks on column Ticker
+
+	//----------------------------------------------------------------------------------------------------
+	// INDEX
+	//----------------------------------------------------------------------------------------------------
+
 	client.Execute("key Stocks Ticker")
-	// Creates tag (non unique index) for table Stocks on column MarketCap
 	client.Execute("tag Stocks MarketCap")
 
 	//----------------------------------------------------------------------------------------------------
-	// SUBSCRIBE 
+	// SUBSCRIBE
 	//----------------------------------------------------------------------------------------------------
-			
+
 	subscriber.Execute("subscribe * from Stocks where MarketCap = 'MEGA CAP'")
 	pubsubid := subscriber.PubSubId()
 	fmt.Println("subscribed to Stocks pubsubid:", pubsubid)
@@ -52,51 +69,48 @@ func main() {
 	// PUBLISH INSERT
 	//----------------------------------------------------------------------------------------------------
 
-	// all commands must be in lowercase string single quotes are optional unless
-	// data contains special characters: whitespace, single quote or comma 
 	client.Execute("insert into Stocks (Ticker, Price, MarketCap) values (GOOG, '1,200.22', 'MEGA CAP')")
 	checkError(client, "insert GOOG failed")
 	client.Execute("insert into Stocks (Ticker, Price, MarketCap) values (MSFT, 38,'MEGA CAP')")
 	checkError(client, "insert MSFT failed")
 
 	//----------------------------------------------------------------------------------------------------
-	// SELECT 
+	// SELECT
 	//----------------------------------------------------------------------------------------------------
 
-	client.Execute("select id, Ticker from Stocks") 
+	client.Execute("select id, Ticker from Stocks")
 	checkError(client, "select failed")
 	for client.NextRow() {
 		fmt.Println("*********************************")
 		fmt.Printf("id:%s Ticker:%s \n", client.Value("id"), client.Value("Ticker"))
 	}
 	checkError(client, "NextRow failed")
-	
+
 	//----------------------------------------------------------------------------------------------------
 	// PROCESS PUBLISHED INSERT
 	//----------------------------------------------------------------------------------------------------
 
-	timeout := 100 
+	timeout := 100
 	for subscriber.WaitForPubSub(timeout) {
 		fmt.Println("*********************************")
-		fmt.Println("Action:", subscriber.Action())		
+		fmt.Println("Action:", subscriber.Action())
 		for subscriber.NextRow() {
 			fmt.Println("New MEGA CAP stock:", subscriber.Value("Ticker"))
 			fmt.Println("Price:", subscriber.Value("Price"))
-		}	
+		}
 		checkError(subscriber, "NextRow failed")
-	}	
+	}
 	checkError(subscriber, "WaitForPubSub failed")
 
 	//----------------------------------------------------------------------------------------------------
 	// PUBLISH UPDATE
 	//----------------------------------------------------------------------------------------------------
 
-	client.Execute("update Stocks set Price = '1,500.00' where Ticker = GOOG") 
+	client.Execute("update Stocks set Price = '1,500.00' where Ticker = GOOG")
 	checkError(client, "update GOOG failed")
 
 	//----------------------------------------------------------------------------------------------------
-	// DOES NOT PUBLISH INSERT
-	// we only subscribed to 'MEGA CAP'
+	// SERVER WILL NOT PUBLISH INSERT BECAUSE WE ONLY SUBSCRIBED TO 'MEGA CAP'
 	//----------------------------------------------------------------------------------------------------
 
 	client.Execute("insert into Stocks (Ticker, Price, MarketCap) values (IBM, 168, 'LARGE CAP')")
@@ -108,7 +122,7 @@ func main() {
 
 	client.Execute("update Stocks set Price = 230.45, MarketCap = 'MEGA CAP' where Ticker = IBM")
 	checkError(client, "update IBM to MEGA CAP failed")
-	
+
 	//----------------------------------------------------------------------------------------------------
 	// PUBLISH REMOVE
 	//----------------------------------------------------------------------------------------------------
@@ -122,33 +136,33 @@ func main() {
 
 	client.Execute("delete from Stocks")
 	checkError(client, "delete failed")
-	
+
 	//----------------------------------------------------------------------------------------------------
-	// PROCESS PUBLISHED ALL 
+	// PROCESS ALL PUBLISHED
 	//----------------------------------------------------------------------------------------------------
 
 	for subscriber.WaitForPubSub(timeout) {
 		fmt.Println("*********************************")
-		fmt.Println("Action:", subscriber.Action())		
+		fmt.Println("Action:", subscriber.Action())
 		for subscriber.NextRow() {
-			for ordinal, column :=  range subscriber.Columns() {
+			for ordinal, column := range subscriber.Columns() {
 				fmt.Printf("%s:%s ", column, subscriber.ValueByOrdinal(ordinal))
 			}
 			fmt.Println("")
-		}	
+		}
 		checkError(subscriber, "NextRow failed")
-	}	
+	}
 	checkError(subscriber, "WaitForPubSub failed")
 
 	//----------------------------------------------------------------------------------------------------
-	// UNSUBSCRIBE 
+	// UNSUBSCRIBE
 	//----------------------------------------------------------------------------------------------------
 
 	subscriber.Execute("unsubscribe from Stocks")
 	checkError(subscriber, "NextRow failed")
-	
+
 	//----------------------------------------------------------------------------------------------------
-	// DISCONNECT 
+	// DISCONNECT
 	//----------------------------------------------------------------------------------------------------
 
 	client.Disconnect()
